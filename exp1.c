ce5d4aa92772db9e815bf11915583e4fcaa8dc68
mithilkunder <mithil.kunder@gmail.com>
2024-10-22 22:29:32 +0530

    exp1.c

--- exp1.c
+++ exp1.c
@@ -6,7 +6,7 @@ void main()
 	int dx,i,j,dy,x1,x,y,y1,x2,y2,xinc,yinc,length;
 	float m;
 	int gd=DETECT,gm;
-	initgraph(&gd,&gm,"C:\YOGISOFT\TC\BGI");
+	initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
 	printf("Enter Starting Co-ordinates");
 	scanf("%d%d",&x1,&y1);
 	printf("Enter End Co-ordinates");
@@ -28,20 +28,17 @@ void main()
 	}
 	xinc=dx/length;
 	yinc=dy/length;
-       //	printf("xinc=%d\n",xinc);
-       //	printf("yinc=%d\n",yinc);
+       
 	putpixel(x1,y1,3);
 	x=x1;
 	y=y1;
-       //	printf("x=%d\n",x);
-       //	printf("y=%d\n",y);
+       
 	for(i=0;i<=length;i++)
 	{
 		x1=x1+xinc;
-	       //	printf("%d\t",x1);
-	       //	printf("\n");
+	       
 		y1=y1+yinc;
-	       //	printf("%d\t",y1);
+	       
 
 		putpixel(x1,y1,4);
 	}
